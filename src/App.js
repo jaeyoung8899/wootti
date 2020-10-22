c61c9c2cc1eee8acaa8279b8aa63bb1e61e4f305
jywoo <wjy1408@gmail.com>
2020-10-22 19:19:17 +0900

    1022

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Link, Route} from 'react-router-dom'
 import './App.css';
+import Home from "./Home";
+import About from "./About";
+import Profile from "./Profile";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <ul>
+            <li>
+                <Link to="/">홈</Link>
+            </li>
+            <li>
+                <Link to="/about">소개</Link>
+            </li>
+            <li>
+                <Link to="/profile/velopert">velopert 프로필</Link>
+            </li>
+            <li>
+                <Link to="/profile/gildong">gildong 프로필</Link>
+            </li>
+        </ul>
+        <hr/>
+      <Route path="/" component={Home} exact={true}/>
+      <Route path={['/about','/info']} component={About}/>
+      <Route path="/profile/:username" component={Profile}/>
     </div>
   );
 }
